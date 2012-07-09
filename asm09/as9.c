8a2f6693db936c867639276d3c91ba6ed584069c
Gordon JC Pearce <gordonjcp@gjcp.net>
2012-07-10 00:05:41 +0100

    assembler initial commit

---
+++ asm09/as9.c
@@ -0,0 +1,15 @@
+#define TRUE (1==1)
+#define FALSE (1==0)
+
+#include <stdio.h>
+#include "as.h"
+#include "table9.h"
+#include "as.c"
+#include "do9.c"
+#include "pseudo.c"
+#include "eval.c"
+#include "symtab.c"
+#include "util.c"
+#include "ffwd.c"
+#include "output.c"
+
