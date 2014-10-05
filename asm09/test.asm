695df7c60a1fbe7e3aa88d96d7951d21fe86051c
Gordon JC Pearce <gordon@gjcp.net>
2014-10-06 00:12:30 +0100

    bit more in the test file

--- asm09/test.asm
+++ asm09/test.asm
@@ -3,3 +3,20 @@ go        lda #'*#
         jsr $8030
         bra go
 	clr $17,U
+
+	org $ba00
+	pshs a,b
+	orcc #$50
+	ldb #$02
+o1	bitb $e100
+	beq o1
+	lda #$f1
+	sta $e101
+o2	bitb $e100
+	beq o2
+	lda 42
+	sta $e101
+	puls b,a
+	andcc #$af
+	rts
+
